bfb2a105f28f0ff682af6fa5380065d4fc3aad97
Garrett Tiller <gtiller24@biomedscienceacademy.org>
2021-01-22 04:12:46 +0000

    I finished the 2 elif, else, and if statements along with the random part.

--- main.py
+++ main.py
@@ -25,14 +25,25 @@ def run_tests():
 def main():
   """This function contains all code for the chatbot."""
   print("Hello!")
-  
+  # This asks a question and stores it as x
   x= str(input("How are you today: "))
   if x == "good" or "great" or "amazing":
     print("Thats good to hear!")
   else: 
     print("I'm sorry to hear that.")
-
-
+  z= str(input("Do you like rollercoasters? "))
+  if z == "yes" or "Yes":
+    print("Thats great, me too")
+  else:
+    print("Oh...")
+  c= str(input("What is one job/place you would like to do/work at?"))
+  r=random.randint(1,3)
+  if r==1:
+    print("Thats so cool, sounds fun!")
+  elif r==2:
+    print("I wouldn't mind that either. ")
+  elif r==3:
+    print("Thats an intresing choice, but thats not my style")
 if __name__ == "__main__":
   main()
   t = input("Run pytest? (y/n)").lower()
